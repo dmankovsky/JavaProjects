3bdc685fb6d25f52ccb6592b29c964609484ce10
dmankovsky <danielmankovsky@gmail.com>
2022-03-30 21:58:33 +0200

    Homeform added (but probably not needed) HomeController updated

--- Udacity/nd035-c1-spring-boot-basics-project-starter/starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/controller/HomeController.java
+++ Udacity/nd035-c1-spring-boot-basics-project-starter/starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/controller/HomeController.java
@@ -1,6 +1,12 @@
 package com.udacity.jwdnd.course1.cloudstorage.controller;
 
+import com.udacity.jwdnd.course1.cloudstorage.model.NoteOverviewForm;
+import com.udacity.jwdnd.course1.cloudstorage.model.SingleNote;
+import com.udacity.jwdnd.course1.cloudstorage.services.NoteService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -8,4 +14,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class HomeController {
     //make equivalent to chat in review project
     // check the template. many functions and buttons there
+
+    //TODO check https://knowledge.udacity.com/questions/783782 and add respective methods
+    @ModelAttribute("newNote")
+    public NoteForm getNoteForm(){
+        return new SingleNote();
+    }
+
+
 }
