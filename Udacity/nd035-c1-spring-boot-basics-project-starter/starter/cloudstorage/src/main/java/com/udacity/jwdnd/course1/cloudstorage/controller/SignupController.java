e845141fb188d02f0b0f30b4d3bc5b9713003269
dmankovsky <danielmankovsky@gmail.com>
2022-04-17 23:44:02 +0200

    Automatic login redirection added to the Signup Controller

--- Udacity/nd035-c1-spring-boot-basics-project-starter/starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/controller/SignupController.java
+++ Udacity/nd035-c1-spring-boot-basics-project-starter/starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/controller/SignupController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequestMapping("/signup")
@@ -24,8 +25,9 @@ public class SignupController {
     }
 
     @PostMapping()
-    public String signupUser(@ModelAttribute User user, Model model) {
+    public String signupUser(@ModelAttribute User user, Model model, RedirectAttributes redirectAttributes) {
         String signupError = null;
+
 
         if (!userService.isUsernameAvailable(user.getUsername())) {
             signupError = "The username already exists.";
@@ -40,6 +42,8 @@ public class SignupController {
 
         if (signupError == null) {
             model.addAttribute("signupSuccess", true);
+            redirectAttributes.addFlashAttribute("SuccessMessage","Sign Up Successfully");
+            return "redirect:/login";
         } else {
             model.addAttribute("signupError", signupError);
         }
