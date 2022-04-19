f1b8f2d47f0624b331033f2f853400f99683356b
Dan <danielmankovskyi@gmail.com>
2022-04-19 21:16:20 +0200

    CredentialMapper added

--- Udacity/nd035-c1-spring-boot-basics-project-starter/starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/mapper/CredentialMapper.java
+++ Udacity/nd035-c1-spring-boot-basics-project-starter/starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/mapper/CredentialMapper.java
@@ -1,9 +1,9 @@
 package com.udacity.jwdnd.course1.cloudstorage.mapper;
 
 import com.udacity.jwdnd.course1.cloudstorage.model.Credential;
-import org.apache.ibatis.annotations.Insert;
-import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Options;
+import org.apache.ibatis.annotations.*;
+
+import java.util.List;
 
 @Mapper
 public interface CredentialMapper {
@@ -15,7 +15,19 @@ public interface CredentialMapper {
     int addCredential(Credential credential);
 
     //editCredential
+    @Update("update credentials set url=#{url}, username=#{username}, " +
+    "key=#{key}, password=#{password} where credentialid=#{credentialid}")
+    int editCredential(Credential credential);
+
     //deleteCredential
+    @Delete("delete from credentials where credentialid=#{credentialid}")
+    int deleteCredential(Integer credentialid);
+
     //isOnlyUsername
+    @Select("select * from credentials where userid=#{userid} and username=#{username}")
+    Credential isOnlyUsername(Integer userid, String username);
+
     //getFromUserId
+    @Select("select * from credentials where userid=#{userid}")
+    List<Credential> getFromUserId(int userid);
 }
