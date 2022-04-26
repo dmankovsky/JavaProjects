aab14124bf01fce4ec28bd89c35779b6a93c136a
Dan <danielmankovskyi@gmail.com>
2022-04-26 23:48:02 +0200

    CredentialService started. addCredential added

---
+++ Udacity/nd035-c1-spring-boot-basics-project-starter/starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/services/CredentialService.java
@@ -0,0 +1,40 @@
+package com.udacity.jwdnd.course1.cloudstorage.services;
+
+import com.udacity.jwdnd.course1.cloudstorage.mapper.CredentialMapper;
+import com.udacity.jwdnd.course1.cloudstorage.model.Credential;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.PostConstruct;
+import java.security.SecureRandom;
+import java.util.Base64;
+
+@Service
+public class CredentialService {
+
+    @Autowired
+    private final CredentialMapper credentialMapper;
+    private final EncryptionService encryptionService;
+
+    public CredentialService(CredentialMapper credentialMapper, EncryptionService encryptionService) {
+        this.credentialMapper = credentialMapper;
+        this.encryptionService = encryptionService;
+    }
+
+    @PostConstruct
+    public void postConstruct(){
+        System.out.println("Creating Credential-Service Bean.");
+    }
+
+    public int addCredential(Credential credential){
+        SecureRandom random = new SecureRandom();
+        byte[] key = new byte[16];
+        random.nextBytes(key);
+        String encodedKey = Base64.getEncoder().encodeToString(key);
+        String encryptedPassword = encryptionService.encryptValue(credential.getPassword(), encodedKey);
+        credential.setKey(encodedKey);
+        credential.setPassword(encryptedPassword);
+        return credentialMapper.addCredential(credential);
+    }
+
+}
